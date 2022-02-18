d6274826d77e6057322b5b5384eab91e83d0164e
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-02-17 18:00:35 -0800

    state properties built and fetched api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+
+import Character from './components/Character'; 
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [ jedi, setJedi ] = useState()
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`)
+    .then(response => {
+      console.log(response.data)
+      setJedi(response.data)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+  }, [] )
+
 
   return (
     <div className="App">
