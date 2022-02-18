fdbad703f80427dc59875fa1f8f00718bf9e6a93
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-02-17 19:31:58 -0800

    rendered names to screen, moving onto styles

--- src/App.js
+++ src/App.js
@@ -10,15 +10,28 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [ jedi, setJedi ] = useState()
+  const [jediButton, setJediButton ] = useState(null)
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+
+  const openDetails = id => {
+    setJediButton(id)
+  }
+
+  const closeDetails = () => {
+    setJediButton(null)
+  }
+
+
 
   useEffect(() => {
     axios.get(`https://swapi.dev/api/people`)
     .then(response => {
       console.log(response.data)
       setJedi(response.data)
+
     })
     .catch(error => {
       console.error(error)
@@ -29,6 +42,15 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+
+    { jedi && jedi.map(info => {
+
+      return <Character info= {info} key= {info.name} openDetails = {openDetails} closeDetails = {closeDetails} /> 
+    } )}
+
+    
+
+
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
 // Write your Character component here
+
+import React from 'react'; 
+
+import styled from 'styled-components'; 
+
+
+const Character = props => {
+    const { info } = props 
+    return (
+        <div className = 'characters' >
+        <p> {info.name}  </p>     
+       
+        </div>
+    )
+}
+
+export default Character; 
